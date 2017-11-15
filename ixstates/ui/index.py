5fe426c531227348ac92856ce739485bf3563aeb
RyanSquared <vandor2012@gmail.com>
2017-11-14 23:32:03 -0600

    ui/index: Render using announcements from SQL

--- ixstates/ui/index.py
+++ ixstates/ui/index.py
@@ -5,15 +5,13 @@ from ixstates import util
 blueprint = flask.Blueprint('ui.index',  # pylint: disable=invalid-name
                             __name__)
 
+APP_QUERY = ("SELECT * FROM announcements LEFT JOIN users ON "
+             "announcements.poster = users.uid ORDER BY announcements.date;")
+
 
 @blueprint.route("/")
 def index():
     "Index page."
-    return util.render_template('ui.index.index', "index.html")
-
-
-@blueprint.route("/login", methods=['POST'])
-def login():
-    "Testing logins"
-    print flask.request.form
-    return util.redirect('ui.index.index')
+    announcements = util.querySQL(APP_QUERY)
+    return util.render_template('ui.index.index', "index.html",
+                                announcements=announcements)
