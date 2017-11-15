bf12cc34c717a9d38206d4e138840dfa8e313ead
RyanSquared <vandor2012@gmail.com>
2017-11-14 23:37:33 -0600

    setup.py: Add markdown for compiling Markdown

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='ixstates',
     version='0.1.0',
     packages=['ixstates', 'ixstates.ui', 'ixstates.api'],
-    install_requires=['bcrypt', 'flask', 'cachetools'],
+    install_requires=['bcrypt', 'flask', 'cachetools', 'markdown'],
     package_data={'ixstates': ['templates/*.html']})
