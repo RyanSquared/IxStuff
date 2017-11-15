73cfe06f4e171324e5c9c5f37c02a222af27acf4
RyanSquared <vandor2012@gmail.com>
2017-11-14 23:27:52 -0600

    setup.py: Add cachetools for caching Markdown generated posts

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='ixstates',
     version='0.1.0',
     packages=['ixstates', 'ixstates.ui', 'ixstates.api'],
-    install_requires=['bcrypt', 'flask'],
+    install_requires=['bcrypt', 'flask', 'cachetools'],
     package_data={'ixstates': ['templates/*.html']})
