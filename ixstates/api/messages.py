9858fef7a2e3d70158670c70860f03a3dbd98cd8
RyanSquared <vandor2012@gmail.com>
2017-11-15 01:06:04 -0600

    api/messages: Use Py2/3 compatible iterables

--- ixstates/api/messages.py
+++ ixstates/api/messages.py
@@ -1,7 +1,11 @@
 "Create a route for sending back messages from a user's session."
-import itertools
-import flask
+try:
+    import itertools.imap as map  # pylint: disable=redefined-builtin
+    import itertools.ifilter as filter  # pylint: disable=redefined-builtin
+except ImportError:  # Python 3
+    pass
 from datetime import datetime
+import flask
 
 blueprint = flask.Blueprint(  # pylint: disable=invalid-name
     "api.messages", __name__)
@@ -12,9 +16,9 @@ def get_messages():
     "Return a JSON list of string messages from `session['messages']`."
     try:
         now = datetime.now()
-        messages = itertools.ifilter(
+        messages = filter(
             lambda x: x[1] > now, flask.session["messages"])
         flask.session["messages"] = []
-        return flask.jsonify(list(itertools.imap(lambda x: x[0], messages)))
+        return flask.jsonify(list(map(lambda x: x[0], messages)))
     except KeyError:
         return flask.jsonify([])
