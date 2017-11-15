6d1c6c34686ae6a378f5b105a453795ce7b048e7
RyanSquared <vandor2012@gmail.com>
2017-11-14 23:29:26 -0600

    flask_app: Add template to render and cache Markdown

--- ixstates/flask_app.py
+++ ixstates/flask_app.py
@@ -3,6 +3,8 @@ import flask
 import ixstates.ui.index
 import ixstates.api.messages
 import ixstates.api.user_management
+import markdown
+from cachetools import func as functools
 
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 app.config['appname'] = "IxStates"
@@ -14,3 +16,10 @@ app.register_blueprint(ixstates.ui.index.blueprint)
 app.register_blueprint(ixstates.api.messages.blueprint, url_prefix="/api")
 app.register_blueprint(
     ixstates.api.user_management.blueprint, url_prefix="/api")
+
+
+@app.template_filter('render_markdown')
+@functools.lru_cache(maxsize=32)
+def render_markdown(content):
+    "Render some Markdown content to HTML."
+    return markdown.markdown(content)
