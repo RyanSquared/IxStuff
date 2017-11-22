b33d42904b2d6f53d12403b1c6ceae6f6c00048b
RyanSquared <vandor2012@gmail.com>
2017-11-21 21:13:00 -0600

    setup: Add Tornado as dependency

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='ixstates',
     version='0.1.0',
     packages=['ixstates', 'ixstates.ui', 'ixstates.api'],
-    install_requires=['bcrypt', 'flask', 'cachetools', 'markdown'],
+    install_requires=['bcrypt', 'flask', 'cachetools', 'markdown', 'tornado'],
     package_data={'ixstates': ['templates/*.html']})
