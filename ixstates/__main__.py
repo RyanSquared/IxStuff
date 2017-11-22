6e96ef9c244f36b41594a777f5dfdd39c9facb99
RyanSquared <vandor2012@gmail.com>
2017-11-22 14:55:52 -0600

    __main__: Initial commit

---
+++ ixstates/__main__.py
@@ -0,0 +1,60 @@
+"Set values for, and start, the Tornado app. "
+# pylint: disable=invalid-name
+import argparse
+import socket
+import tornado.log
+from tornado.ioloop import IOLoop
+from ixstates.tornado_app import make_server
+
+
+def is_socket_addr(address):
+    "Check if an address is valid IPv4 or IPv6"
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+        return address
+    except socket.error:  # not a valid address
+        try:
+            socket.inet_pton(socket.AF_INET6, address)
+        except socket.error:
+            raise argparse.ArgumentTypeError(
+                "%s is not a valid host address" % repr(address))
+    return address
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="ixstates",
+        description="Set values for, and start, the Tornado app.")
+    parser.add_argument(
+        "--port", "-p", help="Server port", type=int,
+        default=5000)
+    parser.add_argument(
+        "--bind", "-b", help="Server bind address", type=is_socket_addr,
+        default="0.0.0.0")
+    parser.add_argument(
+        "--certfile", help="TLS Certificate File (PEM), requires --keyfile")
+    parser.add_argument(
+        "--keyfile", help="TLS Private Key File (PEM), requires --certfile")
+    parser.add_argument("-v", help="Enable Tornado verbose logging",
+                        action="store_true")
+    args = parser.parse_args()
+    if args.v is not None:
+        tornado.log.enable_pretty_logging()
+
+    if args.certfile and args.keyfile:  # Both exist
+        tls_options = {
+            "certfile": args.certfile,
+            "keyfile": args.keyfile}
+    elif args.certfile or args.keyfile:  # Either exist, but not both
+        parser.print_usage()
+        raise SystemExit()
+    else:
+        tls_options = None
+
+    if tls_options is None:
+        http_server = make_server()
+    else:
+        http_server = make_server(ssl_options=tls_options)
+
+    http_server.listen(args.port)
+    IOLoop.instance().start()
