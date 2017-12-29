42ddb61f85996fe79ae9e18bc4147b2cdc44ed77
RyanSquared <vandor2012@gmail.com>
2017-12-28 23:18:08 -0600

    util: Add more utility functions, use Arrow, etc.

--- ixstates/util.py
+++ ixstates/util.py
@@ -1,6 +1,7 @@
 "Utility library for IxStates."
-from datetime import datetime, date, timedelta
+import functools
 import sqlite3
+import arrow
 import flask
 import htmlmin
 
@@ -11,12 +12,34 @@ minifier = htmlmin.Minifier(  # pylint: disable=invalid-name
 DB_CON = sqlite3.connect(":memory:", check_same_thread=False)
 
 
+def requires_login(function):
+    "Raise 403 if given a route without being logged in."
+    @functools.wraps(function)
+    def subfunction(*args, **kwargs):
+        "-"
+        if flask.session.get("uid") is None:
+            flask.abort(403)
+        return function(*args, **kwargs)
+    return subfunction
+
+
+def requires_admin(function):
+    "Decorator for checking for admin status before rendering page"
+    @functools.wraps(function)
+    def subfunction(*args, **kwargs):
+        "-"
+        if not flask.session.get("admin", False):
+            flask.abort(403)
+        return function(*args, **kwargs)
+    return subfunction
+
+
 def queue_message(message):
     "Add a string message to a `session` list."
     if "messages" not in flask.session:
         flask.session["messages"] = []
     flask.session["messages"].append(
-        (message, datetime.now() + timedelta(seconds=10)))
+        (message, arrow.get().shift(seconds=10).datetime))
     flask.session.modified = True
 
 
@@ -27,10 +50,16 @@ def redirect(url):
     return flask.redirect(flask.url_for(url))
 
 
+def render_template_noredir(template, **kwargs):
+    "Render and minify a template."
+    # return minifier.minify(flask.render_template(template, **kwargs))
+    return flask.render_template(template, **kwargs)
+
+
 def render_template(url, template, **kwargs):
     "Set a session URL variable and render a template."
     flask.session['url'] = url
-    return minifier.minify(flask.render_template(template, **kwargs))
+    return render_template_noredir(template, **kwargs)
 
 
 def executeSQL(query, args=None):  # pylint: disable=invalid-name
@@ -63,6 +92,10 @@ CREATE TABLE IF NOT EXISTS users (
 executeSQL("""
 INSERT INTO users (username, password, admin) VALUES ('RyanSquared',
 '$2b$12$bGLD75o2VfH4/YbaR/NSNuolyzS1Rfukcw80P7Xhj2ygDS0HyxUcy', 1);""")
+executeSQL("""
+INSERT INTO users (username, password, admin) VALUES ('Burgbb',
+'$2b$12$bGLD75o2VfH4/YbaR/NSNuolyzS1Rfukcw80P7Xhj2ygDS0HyxUcy', 0);""")
+
 
 executeSQL("""
 CREATE TABLE IF NOT EXISTS announcements (
@@ -76,7 +109,7 @@ CREATE TABLE IF NOT EXISTS announcements (
 executeSQL("""
 INSERT INTO announcements (poster, date, title, content) VALUES (
     1, ?, 'IxRebranding', ?
-);""", (date.today(), """
+);""", (arrow.get().datetime, """
 Ixnay has been a region for many years, but this fuckin bitch ass cunt
 we call Heku has a tad bit of an issue and wants to start redoing some
 things. The forums that are currently being used aren't a good way to
@@ -99,3 +132,46 @@ feel free to join us on IRC.
 
 Do you have an idea that can Make Ixnay Great Again? Feel free to send any
 requests to **LordRyan** on the Ixnay IRC channel."""))
+
+executeSQL("""
+CREATE TABLE IF NOT EXISTS lorewards (
+    uid INTEGER PRIMARY KEY AUTOINCREMENT,
+    giver INTEGER NOT NULL, -- Links to `users.uid`
+    receiver INTEGER NOT NULL, -- Links to `users.uid`
+    date INTEGER NOT NULL,
+    type TINYINT NOT NULL,
+    UNIQUE(date, type) -- No more than once per day
+);""")
+
+executeSQL("""
+INSERT INTO lorewards (giver, receiver, date, type)
+VALUES (1, 1, 1513814400, 1);""")
+
+executeSQL("""
+CREATE TABLE IF NOT EXISTS mails (
+    uid INTEGER PRIMARY KEY AUTOINCREMENT,
+    subject VARCHAR NOT NULL,
+    archived TINYINT DEFAULT 0
+);""")
+
+executeSQL("""
+CREATE TABLE IF NOT EXISTS users2mails (
+    mail_uid INTEGER NOT NULL,
+    user_uid INTEGER NOT NULL
+);""")
+
+executeSQL("""
+CREATE TABLE IF NOT EXISTS messages2mails (
+    mail_uid INTEGER NOT NULL,
+    user_uid INTEGER NOT NULL,
+    text VARCHAR NOT NULL,
+    date INTEGER NOT NULL
+);""")
+
+executeSQL("INSERT INTO mails (subject) VALUES ('Testing Message');")
+executeSQL("INSERT INTO users2mails (mail_uid, user_uid) VALUES (1, 1);")
+executeSQL("INSERT INTO users2mails (mail_uid, user_uid) VALUES (1, 2);")
+executeSQL("""INSERT INTO messages2mails (mail_uid, user_uid, text, date)
+VALUES (1, 2, 'Work on IxStates', 1514390239);""")
+executeSQL("""INSERT INTO messages2mails (mail_uid, user_uid, text, date)
+VALUES (1, 1, 'no u fgt', 1514390253);""")
