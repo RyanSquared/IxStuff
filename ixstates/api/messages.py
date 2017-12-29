14d1e9a3761588728bd92b5273bab013c886dc7e
RyanSquared <vandor2012@gmail.com>
2017-12-28 23:16:16 -0600

    api/messages: Use Arrow for timekeeping

--- ixstates/api/messages.py
+++ ixstates/api/messages.py
@@ -1,5 +1,5 @@
 "Create a route for sending back messages from a user's session."
-from datetime import datetime
+import arrow
 import flask
 
 blueprint = flask.Blueprint(  # pylint: disable=invalid-name
@@ -10,9 +10,9 @@ blueprint = flask.Blueprint(  # pylint: disable=invalid-name
 def get_messages():
     "Return a JSON list of string messages from `session['messages']`."
     try:
-        now = datetime.now()
+        now = arrow.get()
         messages = (x for x in flask.session["messages"]
-                    if x[1] > now)
+                    if arrow.get(x[1]) > now)
         flask.session["messages"] = []
         return flask.jsonify([x[0] for x in messages])
     except KeyError:
