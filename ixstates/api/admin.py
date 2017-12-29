6f6569e8432a17137b5433a7ea786185a1ac8eff
RyanSquared <vandor2012@gmail.com>
2017-12-28 23:19:07 -0600

    api/admin: Initial commit

---
+++ ixstates/api/admin.py
@@ -0,0 +1,45 @@
+"Routes for managing admin accounts"
+import flask
+from ixstates import util
+
+blueprint = flask.Blueprint(  # pylint: disable=invalid-name
+    "api.admin", __name__)
+
+
+@blueprint.route("/give_admin", methods=["POST"])
+@util.requires_admin
+def give_admin():
+    "Give administrative power to a user."
+    try:
+        user = flask.request.form["admin_user"]
+        next(util.querySQL("SELECT uid FROM users WHERE uid = ?;", (user,)))
+        util.executeSQL("UPDATE users SET admin = 1 WHERE uid = ?;", (user,))
+        username = next(util.querySQL(
+            "SELECT username FROM users WHERE uid = ?;", (user,)))["username"]
+        util.queue_message("Updated admin status for user %r." % username)
+    except StopIteration:
+        util.queue_message("User not found. This should not happen. UI bug.")
+    except KeyError:
+        util.queue_message("UID not passed. This should not happen. UI bug.")
+    return util.redirect("ui.admin.index")
+
+
+@blueprint.route("/ban", methods=["POST"])
+@util.requires_admin
+def ban():
+    "Remove login access for a user."
+    # ::TODO::
+    """
+    try:
+        user = flask.request.form["admin_user"]
+        next(util.querySQL("SELECT uid FROM users WHERE uid = ?;", (user,)))
+        util.executeSQL("UPDATE users SET admin = 1 WHERE uid = ?;", (user,))
+        username = next(util.querySQL(
+            "SELECT username FROM users WHERE uid = ?;", (user,)))["username"]
+        util.queue_message("Updated admin status for user %r." % username)
+    except StopIteration:
+        util.queue_message("User not found. This should not happen. UI bug.")
+    except KeyError:
+        util.queue_message("UID not passed. This should not happen. UI bug.")
+    """
+    return util.redirect("ui.admin.index")
