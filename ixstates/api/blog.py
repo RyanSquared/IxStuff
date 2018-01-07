20ab520c722026e1daa94d60e82e3483b1529ca6
RyanSquared <vandor2012@gmail.com>
2018-01-06 20:06:08 -0600

    api/blog: Fix SQL constraint... Kinda. Don't count seconds on post date/time

--- ixstates/api/blog.py
+++ ixstates/api/blog.py
@@ -1,4 +1,5 @@
 "Module for managing user accounts - registration and logins."
+import sqlite3
 import arrow
 import flask
 from ixstates import util
@@ -16,7 +17,11 @@ def post():
     "End route for user logins."
     title = flask.request.form["post_title"]
     body = flask.request.form["post_body"]
-    util.executeSQL(NEW_POST_QUERY,
-                    [flask.session["uid"], arrow.now().timestamp, title, body])
-    util.queue_message("New post made: %r" % title)
+    timestamp = arrow.now().replace(second=0).timestamp
+    try:
+        util.executeSQL(NEW_POST_QUERY,
+                        [flask.session["uid"], timestamp, title, body])
+        util.queue_message("New post made: %r" % title)
+    except sqlite3.IntegrityError:
+        util.queue_message("Post failed: %r; too many in one minute" % title)
     return util.redirect("ui.index.index")
