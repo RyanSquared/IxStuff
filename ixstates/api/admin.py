0164a48e10da01e4e434b79e7d65b95fb15086ca
RyanSquared <vandor2012@gmail.com>
2018-01-09 15:02:58 -0600

    ixstates/api/admin: Add support for banning users

--- ixstates/api/admin.py
+++ ixstates/api/admin.py
@@ -28,18 +28,16 @@ def give_admin():
 @util.requires_admin
 def ban():
     "Remove login access for a user."
-    # ::TODO::
-    """
     try:
-        user = flask.request.form["admin_user"]
-        next(util.querySQL("SELECT uid FROM users WHERE uid = ?;", (user,)))
-        util.executeSQL("UPDATE users SET admin = 1 WHERE uid = ?;", (user,))
+        user = flask.request.form["banned_user"]
         username = next(util.querySQL(
-            "SELECT username FROM users WHERE uid = ?;", (user,)))["username"]
-        util.queue_message("Updated admin status for user %r." % username)
+            "SELECT uid, username FROM users WHERE uid = ?;",
+            (user,)))["username"]
+        util.executeSQL("INSERT INTO bans (user_uid, reason) VALUES (?, ?);",
+                        (user, flask.request.form["reason"]))
+        util.queue_message("User %r has been banned." % username)
     except StopIteration:
         util.queue_message("User not found. This should not happen. UI bug.")
     except KeyError:
         util.queue_message("UID not passed. This should not happen. UI bug.")
-    """
     return util.redirect("ui.admin.index")
